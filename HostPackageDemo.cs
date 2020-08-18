7df10e000ed8665188a0fca548e839b1e98e61b9
Abel Wang <abel.wang@microsoft.com>
2020-08-18 09:52:50 -0700

    adding everything

---
+++ HostPackageDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HostPackageDemo
+{
+    public class HostPackageDemo
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
